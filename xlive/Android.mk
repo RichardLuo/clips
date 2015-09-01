78c9e07ce628c5358511d5096124b402a488197f
Richard Luo <cdominant7@gmail.com>
2015-09-01 11:33:39 +0800

    added the factory scenes

--- xlive/Android.mk
+++ xlive/Android.mk
@@ -8,8 +8,10 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+PATH_ZB_CONFIGS := $(PRODUCT_OUT)/root/zb/configs
 PATH_CLIPS_SOURCE := $(PRODUCT_OUT)/root/zb/clips
 
 $(shell mkdir -p $(PATH_CLIPS_SOURCE))
 $(shell cp $(LOCAL_PATH)/*.clp $(PATH_CLIPS_SOURCE))
+$(shell cp $(LOCAL_PATH)/scene@* $(PATH_ZB_CONFIGS))
 
