940c3b494d7beee04545e9c1abb719faa242cfcc
rxwen <rx.wen218@gmail.com>
2016-03-14 18:47:27 +0800

    place default clips & scenes cfg files to system paprtition

--- xlive/Android.mk
+++ xlive/Android.mk
@@ -10,10 +10,15 @@ include $(CLEAR_VARS)
 
 PATH_ZB_CONFIGS := $(PRODUCT_OUT)/data/zb/configs
 PATH_CLIPS_SOURCE := $(PRODUCT_OUT)/data/zb/clips
+PATH_DEFAULT_ZB_CONFIGS := $(PRODUCT_OUT)/system/data/zb/configs
+PATH_DEFAULT_CLIPS_SOURCE := $(PRODUCT_OUT)/system/data/zb/clips
 
 $(shell mkdir -p $(PATH_ZB_CONFIGS))
 $(shell mkdir -p $(PATH_CLIPS_SOURCE))
+$(shell mkdir -p $(PATH_DEFAULT_ZB_CONFIGS))
+$(shell mkdir -p $(PATH_DEFAULT_CLIPS_SOURCE))
 
 $(shell cp $(LOCAL_PATH)/*.clp $(PATH_CLIPS_SOURCE))
 $(shell cp $(LOCAL_PATH)/scene@* $(PATH_ZB_CONFIGS))
-
+$(shell cp $(LOCAL_PATH)/*.clp $(PATH_DEFAULT_CLIPS_SOURCE))
+$(shell cp $(LOCAL_PATH)/scene@* $(PATH_DEFAULT_ZB_CONFIGS))
