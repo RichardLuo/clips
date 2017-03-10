03169d773da5dd0b350bc1ee08db176794afe3f2
Richard Luo <cdominant7@gmail.com>
2017-03-10 12:55:45 +0800

    fixed path, this commit is belong to Raymond

--- xlive/Android.mk
+++ xlive/Android.mk
@@ -8,8 +8,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-PATH_CLIPS_SOURCE := $(PRODUCT_OUT)/data/zb/clips
-PATH_DEFAULT_CLIPS_SOURCE := $(PRODUCT_OUT)/system/data/zb/clips
+PATH_CLIPS_SOURCE := $(PRODUCT_OUT)/data/xlive/vds/clips
+PATH_DEFAULT_CLIPS_SOURCE := $(PRODUCT_OUT)/system/data/xlive/vds/clips
 
 $(shell mkdir -p $(PATH_CLIPS_SOURCE))
 $(shell mkdir -p $(PATH_DEFAULT_CLIPS_SOURCE))
